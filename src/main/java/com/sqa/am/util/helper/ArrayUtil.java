8a722740c533a2653d22afcf8de2abd1a6f56a05
amusick <annettemusick@gmail.com>
2016-02-10 19:01:16 -0800

    Add Info and Array Util classes to util.helper for automation framework

---
+++ src/main/java/com/sqa/am/util/helper/ArrayUtil.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: ArrayUtil.java<br>
+ *
+ *   Musick, Annette<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Feb 10, 2016
+ *   
+ */
+
+package com.sqa.am.util.helper;
+
+
+/**
+ * ArrayUtil //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Musick, Annette
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class ArrayUtil {
+}
---
+++ src/main/java/com/sqa/am/util/helper/Info.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: Info.java<br>
+ *
+ *   Musick, Annette<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Feb 10, 2016
+ *   
+ */
+
+package com.sqa.am.util.helper;
+
+
+/**
+ * Info //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Musick, Annette
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class Info {
+}
