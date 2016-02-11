c163ba7cf38c417c8b68b6300f0adc9038d8533d
amusick <annettemusick@gmail.com>
2016-02-10 19:05:35 -0800

    Add Methods to Info class for Util.helper

--- src/main/java/com/sqa/am/util/helper/Info.java
+++ src/main/java/com/sqa/am/util/helper/Info.java
@@ -1,15 +1,11 @@
 /**
- *   File Name: Info.java<br>
- *
- *   Musick, Annette<br>
- *   Java Boot Camp Exercise<br>
- *   Instructor: Jean-francois Nepton<br>
- *   Created: Feb 10, 2016
- *   
+ * File Name: Info.java<br>
+ * Musick, Annette<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Feb 10, 2016
  */
-
 package com.sqa.am.util.helper;
-
 
 /**
  * Info //ADDD (description of class)
@@ -17,11 +13,33 @@ package com.sqa.am.util.helper;
  * //ADDD (description of core fields)
  * <p>
  * //ADDD (description of core methods)
- * 
- * @author      Musick, Annette
- * @version     1.0.0
- * @since       1.0
  *
+ * @author Musick, Annette
+ * @version 1.0.0
+ * @since 1.0
  */
 public class Info {
+
+	public static void display2DInfo(Object[][] objects) {
+		// TODO implement method to get2DInfo
+	}
+
+	// Methods which call Object methods getClass().getSimpleName(), toString(),
+	// and in a readable form both to display and get String value of Object
+	public static void displayInfo(Object obj) {
+	}
+
+	public static void displayInfo(Object obj1, Object obj2, Object... objN) {
+	}
+
+	public static void displayInfo(Object[] obj) {
+	}
+
+	public static String get2DInfo(Object[][] objects) {
+		return null;
+	}
+
+	public static String getInfo(Object obj) {
+		return null;
+	}
 }
