cceace48e508679abbd32f3ee24a0bcfe3bc2eaa
amusick <annettemusick@gmail.com>
2016-03-28 16:34:34 -0700

    Udemy While Loop

---
+++ src/main/java/Application2.java
@@ -0,0 +1,29 @@
+/**
+ * File Name: Application2.java<br>
+ * Musick, Annette<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Mar 28, 2016
+ */
+/**
+ * Application2 //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Musick, Annette
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Application2 {
+
+	public static void main(String[] args) {
+		int value = 0;
+		boolean loop = value < 20;
+		while (value < 10) {
+			System.out.println("Hello" + value);
+			value = value + 1;
+		}
+	}
+}
