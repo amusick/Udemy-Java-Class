66d94576801043859929135bd1fc55159bb6cdcc
amusick <annettemusick@gmail.com>
2016-03-28 11:58:52 -0700

    Hello World Udemy

---
+++ src/main/java/com/sqa/am/udemy/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.sqa.am.udemy;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		// TODO Auto-generated method stub
+	}
+}
