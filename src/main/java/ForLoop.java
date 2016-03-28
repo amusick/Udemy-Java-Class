b77595ff81dad1249183bff9d753157be0a6fcda
amusick <annettemusick@gmail.com>
2016-03-28 16:46:47 -0700

    Udemy For Loop

---
+++ src/main/java/ForLoop.java
@@ -0,0 +1,27 @@
+/**
+ * File Name: ForLoop.java<br>
+ * Musick, Annette<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Mar 28, 2016
+ */
+/**
+ * ForLoop //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Musick, Annette
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class ForLoop {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			// System.out.println("Hello " + i);
+			System.out.printf("The value of i is: %d\n", i);
+		}
+	}
+}
