e37f7900a917c0fb520cfff89f97c6d2b53189d8
amusick <annettemusick@gmail.com>
2016-03-31 21:28:20 -0700

    Adding in Methods Homework from SQA

---
+++ src/main/java/com/sqa/am/udemy/Methods.java
@@ -0,0 +1,107 @@
+package com.sqa.am.udemy;
+
+/**
+ * File Name: Methods.java<br>
+ * Musick, Annette<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Mar 31, 2016
+ */
+/**
+ * Methods //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Musick, Annette
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Methods {
+
+	public static void main(String[] args) {
+		welcomeUser();
+		requestUserDetails();
+		while (userRequestsMoreItems()) {
+			searchForItem();
+			addItemToCart();
+		}
+		checkoutUser();
+		calcTotalCost();
+		submitPayment();
+		shipUserItems();
+		closeUserSession();
+	}
+
+	/**
+	 *
+	 */
+	private static void addItemToCart() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 *
+	 */
+	private static void calcTotalCost() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 *
+	 */
+	private static void checkoutUser() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 *
+	 */
+	private static void closeUserSession() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 *
+	 */
+	private static void requestUserDetails() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 *
+	 */
+	private static void searchForItem() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 *
+	 */
+	private static void shipUserItems() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 *
+	 */
+	private static void submitPayment() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @return
+	 */
+	private static boolean userRequestsMoreItems() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 *
+	 */
+	private static void welcomeUser() {
+		// TODO Auto-generated method stub
+	}
+}
