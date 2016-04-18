7a82ae3b3310ac4490af7ae285d1d7059a60cc75
Annette Musick <amusick@ziprealty.com>
2016-04-18 15:27:24 -0700

    Basic Constructor

---
+++ src/main/java/com/sqa/am/udemy/MachineApp.java
@@ -0,0 +1,33 @@
+/**
+ * File Name: MachineApp.java<br>
+ * Musick, Annette<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Apr 18, 2016
+ */
+package com.sqa.am.udemy;
+
+public class MachineApp {
+
+	public static void main(String[] args) {
+		Machine machine1 = new Machine();
+	}
+}
+
+/**
+ * MachineApp //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Musick, Annette
+ * @version 1.0.0
+ * @since 1.0
+ */
+class Machine {
+
+	public Machine() {
+		System.out.println("Constructor Running");
+	}
+}
