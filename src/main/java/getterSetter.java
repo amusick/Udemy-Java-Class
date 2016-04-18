7dc2c1108b6ced00818dc91d6c6f840a94d56c4e
Annette Musick <amusick@ziprealty.com>
2016-04-18 14:38:40 -0700

    Learning get methods

---
+++ src/main/java/getterSetter.java
@@ -0,0 +1,30 @@
+public class getterSetter {
+
+	public static void main(String[] args) {
+		Person person1 = new Person();
+		person1.name = "Joe";
+		person1.age = 25;
+		// person1.speak();
+		int years = person1.calculateYearsToRetirement();
+		System.out.println("Years to retirements: " + years);
+		int age = person1.getAge();
+		System.out.println("Your current age is: " + age);
+		System.out.println("Your name is: ");
+	}
+}
+
+class Person {
+
+	String name;
+
+	int age;
+
+	int calculateYearsToRetirement() {
+		int yearsLeft = 65 - this.age;
+		return yearsLeft;
+	}
+
+	int getAge() {
+		return this.age;
+	}
+}
